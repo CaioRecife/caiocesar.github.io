183473ce06cc3363724df7d35b6798f646866f9a
Caio César <br.caiorecife@cesarleitao.com.br>
2019-07-11 01:06:36 -0300

    night mode

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function nightmode(){
+    var colorbody = document.getElementById("colorbody")
+    var color = document.getElementById("color")
+    var night = document.getElementById("nightmodeimg")
+    var sun = document.getElementById("sunmodeimg")
+
+    colorbody.style.backgroundColor = "#272727"
+    color.style.backgroundColor = "#121212"
+    night.style.display = "none"
+    sun.style.display = "block"
+    
+}
+
+function sunmode(){
+    var colorbody = document.getElementById("colorbody")
+    var color = document.getElementById("color")
+    var night = document.getElementById("nightmodeimg")
+    var sun = document.getElementById("sunmodeimg")
+
+    colorbody.style.backgroundColor = "#ddd"
+    color.style.backgroundColor = "#fff"
+    night.style.display = "block"
+    sun.style.display = "none"
+    
+}
